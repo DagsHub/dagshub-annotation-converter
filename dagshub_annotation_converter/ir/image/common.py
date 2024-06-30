eb51b4d1762c2e563bbd85b8beab72b35204e0bf
KBolashev <kirill@dagshub.com>
2024-06-30 16:23:17 +0300

    Add docstrings for the NormalizationState enum

--- dagshub_annotation_converter/ir/image/common.py
+++ dagshub_annotation_converter/ir/image/common.py
@@ -2,5 +2,9 @@ from enum import Enum
 
 
 class NormalizationState(Enum):
+    """Defines normalization of the annotation values"""
+
     NORMALIZED = (0,)
+    """ Annotation values are in between 0 and 1 relative to the image dimensions """
     DENORMALIZED = (1,)
+    """ Annotation values are in absolute pixel values """
