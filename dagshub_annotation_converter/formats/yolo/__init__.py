dcfdd2b12042135deb6e6491a6118232276c282b
KBolashev <kirill@dagshub.com>
2024-07-08 14:47:04 +0300

    Add import_yolo_result to the top module yolo exports

--- dagshub_annotation_converter/formats/yolo/__init__.py
+++ dagshub_annotation_converter/formats/yolo/__init__.py
@@ -3,3 +3,4 @@ from .context import YoloContext, YoloAnnotationTypes, YoloConverterFunction
 from .pose import export_pose, import_pose_from_string, import_pose_2dim, import_pose_3dim
 from .segmentation import export_segmentation, import_segmentation, import_segmentation_from_string
 from .common import allowed_annotation_types, export_lookup, import_lookup
+from .result_import import import_yolo_result
