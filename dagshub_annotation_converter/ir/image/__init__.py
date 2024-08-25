017629540287e82abd514b56d1a1997b603dd164
KBolashev <kirill@dagshub.com>
2024-08-25 13:47:17 +0300

    Lint fixes

--- dagshub_annotation_converter/ir/image/__init__.py
+++ dagshub_annotation_converter/ir/image/__init__.py
@@ -3,3 +3,13 @@ from .annotations.base import IRImageAnnotationBase
 from .annotations.pose import IRPoseImageAnnotation, IRPosePoint
 from .annotations.segmentation import IRSegmentationImageAnnotation, IRSegmentationPoint
 from .annotations.bbox import IRBBoxImageAnnotation
+
+__all__ = [
+    "CoordinateStyle",
+    "IRImageAnnotationBase",
+    "IRPoseImageAnnotation",
+    "IRPosePoint",
+    "IRSegmentationImageAnnotation",
+    "IRSegmentationPoint",
+    "IRBBoxImageAnnotation",
+]
