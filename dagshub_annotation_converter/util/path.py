00914f4a7d47579f0341e5866cdbe61ad88f8b91
KBolashev <kirill@dagshub.com>
2024-08-25 14:07:16 +0300

    Better docs for util.path.replace_folder

--- dagshub_annotation_converter/util/path.py
+++ dagshub_annotation_converter/util/path.py
@@ -11,9 +11,12 @@ def get_extension(path: Path) -> Optional[str]:
 
 
 def replace_folder(
-    in_path: Path, to_replace: str, replace_with: str, label_extension: Optional[str] = ".txt"
+    in_path: Path, to_replace: str, replace_with: str, replace_extension_with: Optional[str] = ".txt"
 ) -> Optional[Path]:
-    """Swaps LAST occurrence of to_replace with replace_with in the path."""
+    """
+    Swaps LAST occurrence of to_replace with replace_with in the path.
+    If replace_extension_with is provided, replaces the extension of the file with this new extension.
+    """
     new_parts = list(in_path.parts)
 
     # Replace last occurrence
@@ -21,10 +24,10 @@ def replace_folder(
         if part == to_replace:
             new_parts[len(new_parts) - i - 1] = replace_with
 
-    if label_extension is not None:
+    if replace_extension_with is not None:
         # Replace the extension
         ext = get_extension(in_path)
         if ext is None:
             return None
-        new_parts[-1] = new_parts[-1].replace(ext, label_extension)
+        new_parts[-1] = new_parts[-1].replace(ext, replace_extension_with)
     return Path(*new_parts)
